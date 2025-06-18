d0ebd0eaf9b0dcebb9d0d20e2d09c33df037e88d
vamseeuxd <vamsee.uxd@gmail.com>
2025-06-18 22:12:52 +0530

    feat: implement expenses list component with sample data and styling

--- src/app/expenses/expenses.ts
+++ src/app/expenses/expenses.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-expenses',
-  imports: [],
+  imports: [MatListModule, MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './expenses.html',
-  styleUrl: './expenses.scss'
+  styleUrl: './expenses.scss',
 })
 export class Expenses {
-
+  expenses = [
+    { name: 'Groceries', amount: 150, date: new Date('2023-10-01') },
+    { name: 'Utilities', amount: 100, date: new Date('2023-10-05') },
+    { name: 'Rent', amount: 1200, date: new Date('2023-10-01') },
+    { name: 'Internet', amount: 60, date: new Date('2023-10-02') },
+    { name: 'Transportation', amount: 80, date: new Date('2023-10-03') },
+    { name: 'Groceries', amount: 150, date: new Date('2023-10-01') },
+    { name: 'Utilities', amount: 100, date: new Date('2023-10-05') },
+    { name: 'Rent', amount: 1200, date: new Date('2023-10-01') },
+    { name: 'Internet', amount: 60, date: new Date('2023-10-02') },
+    { name: 'Transportation', amount: 80, date: new Date('2023-10-03') },
+    { name: 'Groceries', amount: 150, date: new Date('2023-10-01') },
+    { name: 'Utilities', amount: 100, date: new Date('2023-10-05') },
+    { name: 'Rent', amount: 1200, date: new Date('2023-10-01') },
+    { name: 'Internet', amount: 60, date: new Date('2023-10-02') },
+    { name: 'Transportation', amount: 80, date: new Date('2023-10-03') },
+    { name: 'Groceries', amount: 150, date: new Date('2023-10-01') },
+    { name: 'Utilities', amount: 100, date: new Date('2023-10-05') },
+    { name: 'Rent', amount: 1200, date: new Date('2023-10-01') },
+    { name: 'Internet', amount: 60, date: new Date('2023-10-02') },
+    { name: 'Transportation', amount: 80, date: new Date('2023-10-03') },
+    { name: 'Groceries', amount: 150, date: new Date('2023-10-01') },
+    { name: 'Utilities', amount: 100, date: new Date('2023-10-05') },
+    { name: 'Rent', amount: 1200, date: new Date('2023-10-01') },
+    { name: 'Internet', amount: 60, date: new Date('2023-10-02') },
+    { name: 'Transportation', amount: 80, date: new Date('2023-10-03') },
+    { name: 'Groceries', amount: 150, date: new Date('2023-10-01') },
+    { name: 'Utilities', amount: 100, date: new Date('2023-10-05') },
+    { name: 'Rent', amount: 1200, date: new Date('2023-10-01') },
+    { name: 'Internet', amount: 60, date: new Date('2023-10-02') },
+    { name: 'Transportation', amount: 80, date: new Date('2023-10-03') },
+  ];
 }
