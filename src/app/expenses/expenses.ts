13bffeb62969c8d0532efe60325a8eddc3c92b3a
vamseeuxd <vamsee.uxd@gmail.com>
2025-06-19 08:02:42 +0530

    feat: add context menu to expenses list items with edit, delete, and clone options

--- src/app/expenses/expenses.ts
+++ src/app/expenses/expenses.ts
@@ -3,10 +3,11 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-expenses',
-  imports: [MatListModule, MatIconModule, MatButtonModule, CommonModule],
+  imports: [MatMenuModule, MatListModule, MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './expenses.html',
   styleUrl: './expenses.scss',
 })
